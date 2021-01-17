c144f3747e0d155c1b0bab704be2cf34ec249797
elvanerdem <elvannerdemm@gmail.com>
2021-01-17 21:55:51 +0300

    changes.

--- app/src/main/java/com/elvanerdem/citydistrict/ui/CityDetailActivity.kt
+++ app/src/main/java/com/elvanerdem/citydistrict/ui/CityDetailActivity.kt
@@ -1,40 +1,58 @@
 package com.elvanerdem.citydistrict.ui
 
+import android.Manifest
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.net.Uri
+import android.opengl.Visibility
+import android.os.Build
 import android.os.Bundle
+import android.view.View
+import android.view.View.GONE
+import android.view.View.VISIBLE
+import android.widget.AdapterView
 import android.widget.ArrayAdapter
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import com.elvanerdem.citydistrict.R
 import com.elvanerdem.citydistrict.model.City
+import com.elvanerdem.citydistrict.utils.REQUEST_PERMISSION_PHONE_CALL
 import com.squareup.moshi.JsonAdapter
 import com.squareup.moshi.Moshi
 import kotlinx.android.synthetic.main.activity_city_detail.*
 import timber.log.Timber
 
-class CityDetailActivity : AppCompatActivity() {
+class CityDetailActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
 
+    private val TAG = MainActivity::class.java.simpleName
+
+    private lateinit var city: City
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_city_detail)
 
-        var city = intent.getStringExtra("City")?.let {  getCityDetail(it) }
-        setUpCityDetail(city)
+        city = intent.getStringExtra("City")?.let {
+            getCityDetail(it)
+        } ?: run {
+            City()
+        }
 
-
-
+        setUpCityDetail()
     }
 
-    private fun getCityDetail(jsonStr: String): City? {
+
+    private fun getCityDetail(jsonStr: String): City {
         val moshi: Moshi = Moshi.Builder().build()
         val adapter: JsonAdapter<City> = moshi.adapter(City::class.java)
-        adapter.fromJson(jsonStr).let { city -> return city }
+        return adapter.fromJson(jsonStr) ?: City()
     }
 
-    private fun setUpCityDetail(city: City?) {
+    private fun setUpCityDetail() {
         var districtNameList = city?.districts?.map { it.cityHallName }?.toMutableList()
-        //districtNameList?.add(0, "Seçiniz")
+        districtNameList?.add(0, "Seçiniz")
 
         city.let {
             Timber.d("City " + city?.name)
@@ -49,20 +67,99 @@ class CityDetailActivity : AppCompatActivity() {
             tvInfo.text = "${city?.info}"
 
             tvCityHallPhone.setOnClickListener {
-                val intent = Intent(Intent.ACTION_CALL)
-                val uri = Uri.parse("tel:" + "0${city?.districts?.get(0)?.cityHallPhone}")
-                intent.data = uri
-                startActivity(intent)
+                checkAndRequestPermission()
             }
 
             districtNameList?.let { it1 ->
                 ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, it1)
             }.also {
                 spDistrict.adapter = it
+                spDistrict.setSelection(0,false)
+                spDistrict.onItemSelectedListener = this
             }
 
+        }
+    }
 
+    private fun checkAndRequestPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            when {
+                ContextCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE) == PackageManager.PERMISSION_GRANTED -> {
+                    makeCall()
+                } shouldShowRequestPermissionRationale(Manifest.permission.CALL_PHONE) -> {
+                    showAlertDialogForPermission()
+                } else -> {
+                    requestPermissions(arrayOf(Manifest.permission.CALL_PHONE), REQUEST_PERMISSION_PHONE_CALL)
+                }
+            }
+        }
+    }
+
+    private fun makeCall(){
+        val intent = Intent(Intent.ACTION_CALL)
+        val uri = Uri.parse("tel:${city?.cityHallPhone}")
+        intent.data = uri
+        startActivity(intent)
+    }
+
+    private fun showAlertDialogForPermission(){
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Gerekli İzin")
+        builder.setMessage("Arama yapabilmeniz için izin vermeniz gerekmektedir.")
+        builder.setIcon(android.R.drawable.ic_dialog_alert)
+        builder.setPositiveButton("Tamam") { dialogInterface, i ->
+            dialogInterface.dismiss()
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                requestPermissions(arrayOf(Manifest.permission.CALL_PHONE), REQUEST_PERMISSION_PHONE_CALL)
+            }
+        }
+        builder.setNegativeButton("İptal") { dialogInterface, i ->
+            dialogInterface.dismiss()
         }
 
+        val alertDialog = builder.create()
+        alertDialog.setCancelable(false)
+        alertDialog.show()
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        when (requestCode) {
+            REQUEST_PERMISSION_PHONE_CALL -> {
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    makeCall()
+                } else {
+                    showAlertDialogForPermission()
+                }
+            }
+        }
+    }
+
+    override fun onNothingSelected(p0: AdapterView<*>?) {
+        Timber.d(TAG, "onNothingSelected")
+    }
+
+    override fun onItemSelected(p0: AdapterView<*>, p1: View?, p2: Int, p3: Long) {
+        if (p2 != 0) {
+            val selectedDistrictName = spDistrict.adapter.getItem(p2) as String
+            val selectedDistrict = city.districts.filter { it.cityHallName.equals(selectedDistrictName, true) }.single()
+            if ("" != selectedDistrict.districtName) {
+                tvDistrictInfo.text = selectedDistrict.info
+                tvDistrictHall.text = "${selectedDistrict.cityHallName.toLowerCase().capitalize()} Belediyesi"
+                tvDistrictHallPhone.text = "0${selectedDistrict.cityHallPhone}"
+                tvDistrictHallEmail.text = selectedDistrict.cityHallEMail
+                tvDistrictHallWeb.text = selectedDistrict.cityHallWeb
+                llDistrictDetail.visibility = VISIBLE
+                tvDistrictInfo.visibility = VISIBLE
+            }
+        } else {
+            llDistrictDetail.visibility = GONE
+            tvDistrictInfo.visibility = GONE
+        }
     }
 }
+
+
+
+
+
+
--- app/src/main/java/com/elvanerdem/citydistrict/utils/Constants.kt
+++ app/src/main/java/com/elvanerdem/citydistrict/utils/Constants.kt
@@ -1,3 +1,4 @@
 package com.elvanerdem.citydistrict.utils
 
 const val SHARED_PREF_KEY = "com.elvanerdem.citydistrict.sharedprefkey"
+const val REQUEST_PERMISSION_PHONE_CALL = 123
